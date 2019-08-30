fc5dd68ea87ba6b83ca97df561291aa0f7975a42
Makaronov <kokiys@yandex.ru>
2019-08-30 13:27:50 +0200

    Initial commit

---
+++ app/src/main/java/com/makaroni/calculatort/MainActivity.java
@@ -0,0 +1,17 @@
+package com.makaroni.calculatort;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button);
+
+    }
+}
